5453d80fd2fc9ce5c80e26e3cda4b98d740494e3
meretamal <meretamal@uc.cl>
2021-09-22 23:36:25 -0300

    feat(artists): add create method

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -3,7 +3,15 @@ const KoaRouter = require('koa-router');
 const router = new KoaRouter();
 
 router.get('artists.new', '/new', async (ctx) => {
-  await ctx.render('artists/new');
+  await ctx.render('artists/new', {
+    submitArtistPath: ctx.router.url('artists.create'),
+  });
+});
+
+router.post('artists.create', '/', async (ctx) => {
+  const artist = ctx.orm.artist.build(ctx.request.body);
+  await artist.save({ fields: ['name', 'origin', 'genres', 'formedAt', 'members'] });
+  ctx.redirect(ctx.router.url('authors.new'));
 });
 
 module.exports = router;
