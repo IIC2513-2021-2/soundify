ea0a39ed3eda4ec12e35656d6af1891be65f97ec
meretamal <meretamal@uc.cl>
2021-09-22 23:03:47 -0300

    feat(artists): add artist model and migration

---
+++ src/migrations/20210923020220-create-artist.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('artists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      origin: {
+        type: Sequelize.STRING
+      },
+      genres: {
+        type: Sequelize.STRING
+      },
+      formedAt: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('artists');
+  }
+};
