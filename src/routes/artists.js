4b2a423f414db143c65ed96235e7bbd6b26dc89f
meretamal <meretamal@uc.cl>
2021-09-22 23:50:28 -0300

    feat(artists): add an artist detail view

--- src/routes/artists.js
+++ src/routes/artists.js
@@ -2,6 +2,12 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.param('id', async (id, ctx, next) => {
+  ctx.state.artist = await ctx.orm.artist.findByPk(id);
+  if (!ctx.state.artist) return ctx.throw(404);
+  return next();
+});
+
 router.get('artists.new', '/new', async (ctx) => {
   await ctx.render('artists/new', {
     submitArtistPath: ctx.router.url('artists.create'),
@@ -18,6 +24,13 @@ router.get('artists.list', '/', async (ctx) => {
   const artists = await ctx.orm.artist.findAll();
   await ctx.render('artists/index', {
     artists,
+  });
+});
+
+router.get('artists.show', '/:id', async (ctx) => {
+  const { artist } = ctx.state;
+  await ctx.render('artists/show', {
+    artist,
   });
 });
 
