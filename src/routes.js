ad645827cf818930b716a9ab1a3e632dd602d02c
Sebastián Vicencio <sivicencio@gmail.com>
2021-10-01 17:07:59 +0200

    feat(static): add header and update layout styles

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,17 @@ const albumsForArtists = require('./routes/albumsForArtists');
 
 const router = new KoaRouter();
 
+router.use(async (ctx, next) => {
+  ctx.state.paths = {
+    index: ctx.router.url('index'),
+    artists: ctx.router.url('artists.list'),
+    albums: ctx.router.url('albums.list'),
+    about: ctx.router.url('index.about'),
+  };
+
+  await next();
+});
+
 router.use('/', index.routes());
 router.use('/artists', artists.routes());
 router.use('/albums', albums.routes());
