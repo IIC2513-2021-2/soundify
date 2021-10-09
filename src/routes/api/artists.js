01fea6896c6bb111d6ec367bd86b876e62867abe
meretamal <meretamal@uc.cl>
2021-10-09 12:56:07 -0300

    feat(artists): add artist detail endpoint

--- src/routes/api/artists.js
+++ src/routes/api/artists.js
@@ -2,9 +2,20 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.param('id', async (id, ctx, next) => {
+  ctx.state.artist = await ctx.orm.artist.findByPk(id);
+  if (!ctx.state.artist) return ctx.throw(404);
+  return next();
+});
+
 router.get('/', async (ctx) => {
   const artists = await ctx.orm.artist.findAll();
   ctx.body = artists;
+});
+
+router.get('/:id', async (ctx) => {
+  const { artist } = ctx.state;
+  ctx.body = artist;
 });
 
 module.exports = router;
