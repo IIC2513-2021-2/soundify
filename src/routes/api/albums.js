e1cc541a533ab58dc80472683b1e1d9f45ff754f
pedroherreraj <pedro.herrera@uc.cl>
2021-11-25 20:52:43 -0300

    add more album endpoints

--- src/routes/api/albums.js
+++ src/routes/api/albums.js
@@ -8,6 +8,26 @@ const AlbumSerializer = new JSONAPISerializer('albums', {
 
 const router = new KoaRouter();
 
+router.param('id', async (id, ctx, next) => {
+  ctx.state.album = await ctx.orm.album.findByPk(id);
+  if (!ctx.state.album) {
+    return ctx.throw(404, "The album you are looking for doesn't exist");
+  }
+  return next();
+});
+
+router.get('api.albums.index', '/', async (ctx) => {
+  const albums = await ctx.orm.album.findAll();
+  ctx.body = AlbumSerializer.serialize(albums);
+});
+
+router.get('api.albums.show', '/:id', async (ctx) => {
+  const { album } = ctx.state;
+  const artist = await album.getArtist();
+  ctx.body = AlbumSerializer.serialize(album);
+  ctx.body.data.attributes.artist = artist.name;
+});
+
 router.post('api.albums.create', '/', async (ctx) => {
   try {
     const album = ctx.orm.album.build(ctx.request.body);
