12c15432fc5194c2cd53e5326154d25bc05ec4bb
pedroherreraj <pedro.herrera@uc.cl>
2021-11-27 15:44:38 -0300

    cambios profe

--- src/routes/api/albums.js
+++ src/routes/api/albums.js
@@ -42,7 +42,7 @@ router.post('api.albums.create', '/', async (ctx) => {
 router.patch('api.albums.update', '/:id', async (ctx) => {
   const { cloudinary } = ctx.state;
   try {
-    const album = await ctx.orm.album.findByPk(ctx.params.id);
+    const album = await ctx.state;
     const { cover } = ctx.request.files;
     if (cover.size > 0) {
       const imageUrl = await cloudinary.uploader.upload(cover.path);
